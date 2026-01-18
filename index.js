872eb7d6959e2a2347514d1e9ec12de0e0fe0a69
asadulhabibasif26 <eloopazbd@gmail.com>
2026-01-18 10:56:25 +0600

    mongodb server connect sussecfully

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require("express");
 
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dwfjgio.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Danif Bus server is running..............");
 });
+
+async function run() {
+  try{
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally{
+
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Danif Bus server is running on port number : `, port);
