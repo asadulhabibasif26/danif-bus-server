8f6431c59385c34698ea1ea3b45bfe77c657485d
asadulhabibasif26 <eloopazbd@gmail.com>
2026-01-19 19:35:06 +0600

    parcel delete api added

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete("/tickets/:id" , async (req , res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await ticketsCollection.deleteOne(query);
+      res.send(result);
+    })
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
